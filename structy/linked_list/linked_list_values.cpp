2582bf0e7ef72ae16c1edb2716ff8a560a1328c1
PopsButNotDad <77769004+PopsButNotDad@users.noreply.github.com>
2022-02-02 13:07:50 -0800

    recursive

--- structy/linked_list/linked_list_values.cpp
+++ structy/linked_list/linked_list_values.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <string>
-#include <iostream>
 
 //Write a function, linkedListValues, that takes in a pointer to the head of a linked list as an argument. The function
 //should return an array containing all values of the nodes in the linked list.
@@ -17,11 +16,31 @@ class Node {
     }
 };
 
-
-
+//iterative
 std::vector<std::string> linkedListValues(Node* head) {
-    // todo
-    return;
+    std::vector<std::string> result;
+    Node* current = head;
+    while(current != nullptr){
+        result.push_back(current->val);
+        current = current->next;
+    }
+    return result;
+};
+
+//USING OVERLOADING HERE, SAME FUNCTION IS NAMED TWICE BUT TAKES DIFFERENT ARGUMENTS
+//recursive
+void linkedListValuesRe(Node* head, std::vector<std::string> &values) {
+    if(head == nullptr){
+        return;
+    }
+    values.push_back(head->val);
+    linkedListValuesRe(head->next, values);
+};
+
+std::vector<std::string> linkedListValuesRe(Node* head) {
+    std::vector<std::string> values;
+    linkedListValuesRe(head,values);
+    return values;
 };
 
 int main() {
@@ -37,6 +56,7 @@ int main() {
     // a -> b -> c -> d
 
     linkedListValues(&a); // -> [ "a", "b", "c", "d" ]
+    linkedListValuesRe(&a); // -> [ "a", "b", "c", "d" ]
 
     Node x("x");
     Node y("y");
@@ -46,14 +66,17 @@ int main() {
     // x -> y
 
     linkedListValues(&x); // -> [ "x", "y" ]
+    linkedListValuesRe(&x); // -> [ "x", "y" ]
 
     Node q("q");
 
     // q
 
     linkedListValues(&q); // -> [ "q" ]
+    linkedListValuesRe(&q); // -> [ "q" ]
 
     linkedListValues(nullptr); // -> [ ]
+    linkedListValuesRe(nullptr); // -> [ ]
 }
 
 //Complexity
