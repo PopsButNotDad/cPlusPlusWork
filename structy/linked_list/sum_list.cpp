bf4108da23c044da7a2aeea86673b1a9138cb03d
PopsButNotDad <77769004+PopsButNotDad@users.noreply.github.com>
2022-02-02 13:09:51 -0800

    Create sum_list.cpp

---
+++ structy/linked_list/sum_list.cpp
@@ -0,0 +1,52 @@
+class Node {
+  public:
+    int val;
+    Node* next;
+
+    Node(int initialVal) {
+      val = initialVal;
+      next = nullptr;
+    }
+};
+
+
+
+int sumList(Node* head) {
+  // todo
+  return;
+}
+
+int main() {
+    Node a(2);
+    Node b(8);
+    Node c(3);
+    Node d(-1);
+    Node e(7);
+
+    a.next = &b;
+    b.next = &c;
+    c.next = &d;
+    d.next = &e;
+
+    // 2 -> 8 -> 3 -> -1 -> 7
+
+    sumList(&a); // 19
+
+    Node x(38);
+    Node y(4);
+
+    x.next = &y;
+
+    // 38 -> 4
+
+    sumList(&x); // 42
+
+    Node z(100);
+
+    // 100
+
+    sumList(&z); // 100
+
+    sumList(nullptr); // 0
+
+}
