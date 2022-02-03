088499b6ea002c60da7530eed2f036b44a03a9bb
PopsButNotDad <77769004+PopsButNotDad@users.noreply.github.com>
2022-02-03 14:03:49 -0800

    Update merge_lists.cpp

--- structy/linked_list/merge_lists.cpp
+++ structy/linked_list/merge_lists.cpp
@@ -23,11 +23,8 @@ class Node {
 Node* mergeLists(Node* head1, Node* head2) {
     Node dummyHead(0);
     Node* tail = &dummyHead;
-
     Node* current1 = head1;
     Node* current2 = head2;
-
-
     while (current1 != nullptr && current2 != nullptr){
         if (current1->val < current2->val){
             tail->next = current1;
@@ -36,22 +33,34 @@ Node* mergeLists(Node* head1, Node* head2) {
             tail->next = current2;
             current2 = current2->next;
         }
-
         tail = tail->next;
     }
-
     if(current1 == nullptr){
         tail->next = current2;
     }
-
     if(current2 == nullptr){
         tail->next = current1;
     }
     return dummyHead.next;
 }
 
+
+//recursive function will return the head of the list so we don't need the dummy head variable like the iterative version
 Node* mergeListsRe(Node* head1, Node* head2) {
-   
+    if(head1 == nullptr){
+        return head2;
+    }
+    if(head2 == nullptr){
+        return head1;
+    }
+
+    if(head1->val < head2->val){
+        head1->next =mergeListsRe(head1->next, head2);
+        return head1;
+    } else {
+        head2->next = mergeListsRe(head1, head2->next);
+        return head2;
+    }
 }
 
 
