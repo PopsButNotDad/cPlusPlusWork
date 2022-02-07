69c4161d9524c082cd863280bc61c5e21145c6d3
PopsButNotDad <77769004+PopsButNotDad@users.noreply.github.com>
2022-02-07 11:37:23 -0800

    Update longest_streak.cpp

--- structy/linked_list/longest_streak.cpp
+++ structy/linked_list/longest_streak.cpp
@@ -15,44 +15,59 @@ class Node {
 
 
 int longestStreak(Node* head) {
-    
-    return;
+    Node* currentNode = head;
+    int maxStreak = 0;
+    int currentStreak = 0;
+    int prevVal = 0;
+    while (currentNode != nullptr){
+        if(prevVal == currentNode->val){
+            currentStreak += 1;
+        } else {
+            currentStreak = 1;
+        }
+        if(currentStreak > maxStreak){
+            maxStreak = currentStreak;
+        }
+        prevVal = currentNode->val;
+        currentNode = currentNode->next;
+    };
+    return maxStreak;
 }
 
 int main() {
-    Node a(5);
-    Node b(5);
-    Node c(7);
-    Node d(7);
-    Node e(7);
-    Node f(6);
+    Node a0(5);
+    Node b0(5);
+    Node c0(7);
+    Node d0(7);
+    Node e0(7);
+    Node f0(6);
 
-    a.next = &b;
-    b.next = &c;
-    c.next = &d;
-    d.next = &e;
-    e.next = &f;
+    a0.next = &b0;
+    b0.next = &c0;
+    c0.next = &d0;
+    d0.next = &e0;
+    e0.next = &f0;
 
     // 5 -> 5 -> 7 -> 7 -> 7 -> 6
 
-    longestStreak(&a); // 3
+    longestStreak(&a0); // 3
 
-    Node a(3);
-    Node b(3);
-    Node c(3);
-    Node d(3);
-    Node e(9);
-    Node f(9);
+    Node a1(3);
+    Node b1(3);
+    Node c1(3);
+    Node d1(3);
+    Node e1(9);
+    Node f1(9);
 
-    a.next = &b;
-    b.next = &c;
-    c.next = &d;
-    d.next = &e;
-    e.next = &f;
+    a1.next = &b1;
+    b1.next = &c1;
+    c1.next = &d1;
+    d1.next = &e1;
+    e1.next = &f1;
 
     // 3 -> 3 -> 3 -> 3 -> 9 -> 9
 
-    longestStreak(&a); // 4
+    longestStreak(&a1); // 4
 
     Node a(9);
     Node b(9);
@@ -71,20 +86,26 @@ int main() {
 
     longestStreak(&a); // 3
 
-    Node a(5);
-    Node b(5);
+    Node a3(5);
+    Node b3(5);
 
-    a.next = &b;
+    a3.next = &b3;
 
     // 5 -> 5
 
-    longestStreak(&a); // 2
+    longestStreak(&a3); // 2
 
-    Node a(4);
+    Node a2(4);
 
     // 4
 
-    longestStreak(&a); // 1
+    longestStreak(&a2); // 1
 
     longestStreak(nullptr); // 0
 };
+
+//Complexity
+    //n = number of nodes 
+
+    //Time: O( n )
+    //Space: O( 1 )
