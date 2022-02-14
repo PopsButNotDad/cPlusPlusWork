5f5dc8c1c8f56b049e35063052c3f25d5e5b4215
PopsButNotDad <77769004+PopsButNotDad@users.noreply.github.com>
2022-02-14 15:14:42 -0800

    Update minimum_island.cpp

--- structy/graph/minimum_island.cpp
+++ structy/graph/minimum_island.cpp
@@ -1,4 +1,6 @@
 #include <vector>
+#include <unordered_set>
+#include <string>
 
 
 //Write a function, minimumIsland, that takes in a grid containing Ws and Ls. W represents water and L represents Land.
@@ -7,9 +9,38 @@
 
 //You may assume that the grid contains at least one island.
 
+int explore(std::vector<std::vector<char>> grid, int r, int c, std::unordered_set<std::string> visited) {
+    bool rowInbounds = 0 <= r && r < grid.size();
+    bool colInbounds = 0 <= c && c < grid.size();
+
+    if(!rowInbounds || !colInbounds){
+        return 0;
+    }
+
+    if(grid[r][c] == 'W'){
+        return 0;
+    }
+
+    std::string pos = std::to_string(r) + "," + std::to_string(c);
+
+    if(visited.count(pos) > 0){
+        return 0;
+    }
+
+    visited.insert(pos);
+
+
+
+}
 
 int minimumIsland(std::vector<std::vector<char>> grid) {
-  // todo
+  std::unordered_set<std::string> visited;
+
+  for(int r = 0; r < grid.size() ; r+=1){
+      for(int c = 0; c < grid[r].size(); c+=1){
+          explore(grid, r, c, visited);
+      }
+  }
   return;
 }
 
