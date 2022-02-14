2c3b014d4a2fe3d1b9956155389a84b6add9f795
PopsButNotDad <77769004+PopsButNotDad@users.noreply.github.com>
2022-02-14 14:15:24 -0800

    Update island_count.cpp

--- structy/graph/island_count.cpp
+++ structy/graph/island_count.cpp
@@ -1,5 +1,8 @@
 #include <vector>
 
+//Write a function, islandCount, that takes in a grid containing Ws and Ls. W represents water and L represents Land. The
+//function should return the number of islands on the grid. An island is a vertically or horizontally connected region of
+//land.
 
 int islandCount(std::vector<std::vector<char>> grid) {
   // todo
@@ -7,5 +10,43 @@ int islandCount(std::vector<std::vector<char>> grid) {
 }
 
 int main() {
-    
+    std::vector<std::vector<char>> grid {
+        {'W', 'L', 'W', 'W', 'W'},
+        {'W', 'L', 'W', 'W', 'W'},
+        {'W', 'W', 'W', 'L', 'W'},
+        {'W', 'W', 'L', 'L', 'W'},
+        {'L', 'W', 'W', 'L', 'L'},
+        {'L', 'L', 'W', 'W', 'W'}
+    };
+    islandCount(grid); // -> 3
+
+    std::vector<std::vector<char>> grid1 {
+        {'L', 'W', 'W', 'L', 'W'},
+        {'L', 'W', 'W', 'L', 'L'},
+        {'W', 'L', 'W', 'L', 'W'},
+        {'W', 'W', 'W', 'W', 'W'},
+        {'W', 'W', 'L', 'L', 'L'}
+    };
+    islandCount(grid1); // -> 4
+
+    std::vector<std::vector<char>> grid2 {
+        {'L', 'L', 'L'},
+        {'L', 'L', 'L'},
+        {'L', 'L', 'L'}
+    };
+    islandCount(grid2); // -> 1
+
+    std::vector<std::vector<char>> grid3 {
+        {'W', 'W'},
+        {'W', 'W'},
+        {'W', 'W'}
+    };
+    islandCount(grid3); // -> 0
 }
+
+//Complexity:
+    //r = number of rows
+    //c = number of columns
+
+    //Time: O( rc )
+    //Space: O( rc )
