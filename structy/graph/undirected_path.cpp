10c4537f3fa0f824ec294535fb94afb1b8d92c42
PopsButNotDad <77769004+PopsButNotDad@users.noreply.github.com>
2022-02-14 12:12:40 -0800

    Create undirected_path.cpp

---
+++ structy/graph/undirected_path.cpp
@@ -0,0 +1,121 @@
+#include <vector>
+#include <tuple>
+#include <string>
+
+//Write a function, undirectedPath, that takes in a vector of edges for an undirected graph and two nodes (nodeA, nodeB).
+//The function should return a boolean indicating whether or not there exists a path between nodeA and nodeB.
+
+
+bool undirectedPath(std::vector<std::tuple<std::string, std::string>> edges, std::string src, std::string dst) {
+    // todo
+    return;
+}
+
+int main() {
+std::vector<std::tuple<std::string, std::string>> edges {
+  {"i", "j"},
+  {"k", "i"},
+  {"m", "k"},
+  {"k", "l"},
+  {"o", "n"}
+};
+
+undirectedPath(edges, "j", "m"); // -> 1 (true)
+
+std::vector<std::tuple<std::string, std::string>> edges1 {
+  {"i", "j"},
+  {"k", "i"},
+  {"m", "k"},
+  {"k", "l"},
+  {"o", "n"}
+};
+
+undirectedPath(edges1, "m", "j"); // -> 1 (true)
+
+std::vector<std::tuple<std::string, std::string>> edges2 {
+  {"i", "j"},
+  {"k", "i"},
+  {"m", "k"},
+  {"k", "l"},
+  {"o", "n"}
+};
+
+undirectedPath(edges2, "l", "j"); // -> 1 (true)
+
+std::vector<std::tuple<std::string, std::string>> edges3 {
+  {"i", "j"},
+  {"k", "i"},
+  {"m", "k"},
+  {"k", "l"},
+  {"o", "n"}
+};
+
+undirectedPath(edges3, "k", "o"); // -> 0 (false)
+
+std::vector<std::tuple<std::string, std::string>> edges4 {
+  {"i", "j"},
+  {"k", "i"},
+  {"m", "k"},
+  {"k", "l"},
+  {"o", "n"}
+};
+
+undirectedPath(edges4, "i", "o"); // -> 0 (false)
+
+std::vector<std::tuple<std::string, std::string>> edges5 {
+  {"b", "a"},
+  {"c", "a"},
+  {"b", "c"},
+  {"q", "r"},
+  {"q", "s"},
+  {"q", "u"},
+  {"q", "t"}
+};
+
+
+undirectedPath(edges5, "a", "b"); // -> 1 (true)
+
+std::vector<std::tuple<std::string, std::string>> edges6 {
+  {"b", "a"},
+  {"c", "a"},
+  {"b", "c"},
+  {"q", "r"},
+  {"q", "s"},
+  {"q", "u"},
+  {"q", "t"}
+};
+
+undirectedPath(edges6, "a", "c"); // -> 1 (true)
+
+std::vector<std::tuple<std::string, std::string>> edges7 {
+  {"b", "a"},
+  {"c", "a"},
+  {"b", "c"},
+  {"q", "r"},
+  {"q", "s"},
+  {"q", "u"},
+  {"q", "t"}
+};
+
+undirectedPath(edges7, "r", "t"); // -> 1 (true)
+
+std::vector<std::tuple<std::string, std::string>> edges8 {
+  {"b", "a"},
+  {"c", "a"},
+  {"b", "c"},
+  {"q", "r"},
+  {"q", "s"},
+  {"q", "u"},
+  {"q", "t"}
+};
+
+undirectedPath(edges8, "r", "b"); // -> 0 (false)
+
+std::vector<std::tuple<std::string, std::string>> edges9 {
+  {"s", "r"},
+  {"t", "q"},
+  {"q", "r"}
+};
+
+undirectedPath(edges9, "r", "t"); // -> 1 (true)
+}
