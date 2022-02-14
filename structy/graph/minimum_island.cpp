4d46b034e2043a234688f84d1bf2dec0a9e27583
PopsButNotDad <77769004+PopsButNotDad@users.noreply.github.com>
2022-02-14 15:28:31 -0800

    Update minimum_island.cpp

--- structy/graph/minimum_island.cpp
+++ structy/graph/minimum_island.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <unordered_set>
 #include <string>
+#include <algorithm>
 
 
 //Write a function, minimumIsland, that takes in a grid containing Ws and Ls. W represents water and L represents Land.
@@ -9,31 +10,45 @@
 
 //You may assume that the grid contains at least one island.
 
-int explore(std::vector<std::vector<char>> grid, int r, int c, std::unordered_set<std::string> visited) {
+int explore(std::vector<std::vector<char>> grid, int r, int c, std::unordered_set<std::string> &visited) {
     bool rowInbounds = 0 <= r && r < grid.size();
-    bool colInbounds = 0 <= c && c < grid.size();
+    bool colInbounds = 0 <= c && c < grid[0].size();
     if(!rowInbounds || !colInbounds){
-        return 0;
+        return -1;
     }
     if(grid[r][c] == 'W'){
-        return 0;
+        return -1;
     }
     std::string pos = std::to_string(r) + "," + std::to_string(c);
     if(visited.count(pos) > 0){
-        return 0;
+        return -1;
     }
     visited.insert(pos);
+
+    int totalSize = 1;
+
+    totalSize += std::max(0, explore(grid, r+1, c, visited));
+    totalSize += std::max(0, explore(grid, r-1, c, visited));
+    totalSize += std::max(0, explore(grid, r, c+1, visited));
+    totalSize += std::max(0, explore(grid, r, c-1, visited));
+
+    return totalSize;
 
 }
 
 int minimumIsland(std::vector<std::vector<char>> grid) {
-  std::unordered_set<std::string> visited;
-  for(int r = 0; r < grid.size() ; r+=1){
-      for(int c = 0; c < grid[r].size(); c+=1){
-          explore(grid, r, c, visited);
-      }
-  }
-  return;
+    int min = 0;
+    std::unordered_set<std::string> visited;
+    for(int r = 0; r < grid.size() ; r+=1){
+        for(int c = 0; c < grid[0].size(); c+=1){
+            int islandSize = explore(grid, r, c, visited);
+
+            if((islandSize != -1) && min == 0 || islandSize < min){
+                min = islandSize;
+            }
+        }
+    }
+    return min;
 }
 
 int main() {
