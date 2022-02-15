2cfec83ddd16f68f4197c316ea6ca4e1e34e7993
PopsButNotDad <77769004+PopsButNotDad@users.noreply.github.com>
2022-02-15 14:51:29 -0800

    Create has_cycle.cpp

---
+++ structy/graph/has_cycle.cpp
@@ -0,0 +1,64 @@
+#include <vector>
+#include <unordered_map>
+
+//Write a function, hasCycle, that takes in an unordered map representing the adjacency list of a directed graph.
+//The function should return a boolean indicating whether or not the graph contains a cycle.
+
+
+bool hasCycle(std::unordered_map<char, std::vector<char>> graph) {
+    // todo
+    return;
+}
+
+int main() {
+    std::unordered_map<char, std::vector<char>> graph {
+        { 'a', {'b'} },
+        { 'b', {'c'} },
+        { 'c', {'a'} }
+    };
+    hasCycle(graph); // -> 1 (true)
+
+    std::unordered_map<char, std::vector<char>> graph1 {
+        { 'a', {'b', 'c'} },
+        { 'b', {'c'} },
+        { 'c', {'d'} },
+        { 'd', {} }
+    };
+    hasCycle(graph1); // -> 0 (false)
+
+    std::unordered_map<char, std::vector<char>> graph2 {
+        { 'a', {'b', 'c'} },
+        { 'b', {} },
+        { 'c', {} },
+        { 'e', {'f'} },
+        { 'f', {'e'} }
+    };
+    hasCycle(graph2); // -> 1 (true)
+
+    std::unordered_map<char, std::vector<char>> graph3 {
+        { 'q', {'r', 's'} },
+        { 'r', {'t', 'u'} },
+        { 's', {} },
+        { 't', {} },
+        { 'u', {} },
+        { 'v', {'w'} },
+        { 'w', {} },
+        { 'x', {'w'} }
+    };
+    hasCycle(graph3); // -> 0 (false)
+
+    std::unordered_map<char, std::vector<char>> graph4 {
+        { 'a', {'b'} },
+        { 'b', {'c'} },
+        { 'c', {'a'} },
+        { 'g', {} }
+    };
+    hasCycle(graph4); // -> 1 (true)
+}
+
+//Complexity
+    //
+    //
+
+    //Time: 
+    //Space: 
