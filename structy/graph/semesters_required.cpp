5bbf507fa607cc8a75e29204828aa492bee8d800
PopsButNotDad <77769004+PopsButNotDad@users.noreply.github.com>
2022-02-15 13:49:13 -0800

    Update semesters_required.cpp

--- structy/graph/semesters_required.cpp
+++ structy/graph/semesters_required.cpp
@@ -13,10 +13,72 @@
 
 //You can assume that it is possible to eventually complete all courses.
 
+//function to create map from edge list, takes in the prereqs/edges vector as an argument
+std::unordered_map<int, std::vector<int>> buildGraph(std::vector<std::tuple<int, int>> edges){
+    //create the adjacency list
+    std::unordered_map<int, std::vector<int>> graph;
+    //for the edge values in the edges list
+    for (auto[a, b] : edges){
+        //push the second value into the graph at the first value
+        graph[a].push_back(b);
+    }
+    //return the graph
+    return graph;
+}
 
+//recursive helper function takes in the graph, reference to distance map, and an int node
+int exploreDistance(std::unordered_map<int, std::vector<int>> graph, std::unordered_map<int, int> &distanceMap, int node){
+    //if the node value is already in the distance map
+    if(distanceMap.count(node) > 0){
+        //return the distance map value at that node
+        return distanceMap[node];
+    }
+    //max value variable
+    int longest = 0;
+    //for all the neighbors in the graph at that node value
+    for(int neighbor : graph[node]){
+        //explore the neighbors and save it to a variable neighborDistance
+        int neighborDistance = exploreDistance(graph, distanceMap, neighbor);
+        //if neighborDistance is larger than the max variable
+        if(neighborDistance > longest){
+            //set max variable to the neighborDistance
+            longest = neighborDistance;
+        }
+    }
+    //the distance map at that node is equal to the max variable plus 1 (include the self)
+    distanceMap[node] = longest + 1;
+    //return the value within the distance map at that node
+    return distanceMap[node];
+
+}
+//semesters required function takes in the list of courses along with the edges (prereqs)
 int semestersRequired(int numCourses, std::vector<std::tuple<int, int>> prereqs) {
-    // todo
-    return;
+    //create adjacency list using function above and call it graph
+    std::unordered_map<int, std::vector<int>> graph = buildGraph(prereqs);
+    //create the distance map
+    std::unordered_map<int, int> distanceMap;
+    //for loop iterating through all the courses
+    for (int node = 0; node < numCourses; node +=1){
+        //if the node has no nexts
+        if(graph[node].size() == 0){
+            //populate the map at that node with 1 (include the self)
+            distanceMap[node] = 1;
+        }
+    }
+    //max value variable
+    int longest = 0;
+    //once again iterate through all the courses
+    for (int node = 0; node < numCourses; node +=1){
+        //set distance variable equal to the helper function ran at that node (recursive leap of faith)
+        int distance = exploreDistance(graph, distanceMap, node); //Todo
+        //if the distance returned is longer than the max variable 
+        if(distance > longest){
+            //set the max variable to that distance
+            longest = distance;
+        }
+    }
+    //return the max variable
+    return longest;
 }
 
 int main() {
