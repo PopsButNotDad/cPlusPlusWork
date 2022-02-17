3e49212cc67d8b95093f068ce52c8cec62cf6a30
PopsButNotDad <77769004+PopsButNotDad@users.noreply.github.com>
2022-02-17 11:07:41 -0800

    fib

--- structy/dynamic_programming/fib.cpp
+++ structy/dynamic_programming/fib.cpp
@@ -1,10 +1,24 @@
+#include <unordered_map>
+
 //Write a function, fib, that takes in a number as an argument, n, and returns the n-th number of the fibonacci sequence.
 //The 0-th number of the sequence is 0. The 1-st number of the sequence is 1. To generate further numbers of the sequence,
 //calculate the sum of the previous two numbers.
 
+int fib(int n, std::unordered_map<int, int> &memo) {
+    if(memo.count(n) > 0){
+        return memo[n];
+    }
+
+    if( n < 2 ){
+        return n;
+    }
+    memo[n] =  fib(n - 1, memo) + fib(n - 2, memo);
+    return memo[n];
+}
+
 int fib(int n) {
-    // todo
-    return;
+    std::unordered_map<int, int> memo;
+    return fib(n, memo);
 }
 
 int main() {
