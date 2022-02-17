62060b99cca4ba21e4b9cc3dc04f335cf715969a
PopsButNotDad <77769004+PopsButNotDad@users.noreply.github.com>
2022-02-17 11:29:14 -0800

    Update tribonacci.cpp

--- structy/dynamic_programming/tribonacci.cpp
+++ structy/dynamic_programming/tribonacci.cpp
@@ -7,9 +7,22 @@
 
 //Solve this recursively.
 
-int tribonacci(int n, std::unordered_map<int, int> memo) {
-    
-    return;
+int tribonacci(int n, std::unordered_map<int, int> &memo) {
+    if(memo.count(n) > 0){
+        return memo[n];
+    }
+
+    if(n == 0 || n == 1){
+        return 0;
+    }
+
+    if(n == 2){
+        return 1;
+    }
+
+    memo[n] = tribonacci(n-1, memo) + tribonacci(n-2, memo) + tribonacci(n-3, memo);
+
+    return memo[n];
 }
 
 int tribonacci(int n) {
