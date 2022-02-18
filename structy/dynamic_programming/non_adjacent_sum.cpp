827f2151cc3dea9a3b9ebebfed0ef3cd193e4ebb
PopsButNotDad <77769004+PopsButNotDad@users.noreply.github.com>
2022-02-18 13:26:56 -0800

    non-adj

--- structy/dynamic_programming/non_adjacent_sum.cpp
+++ structy/dynamic_programming/non_adjacent_sum.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 
 //Write a function, nonAdjacentSum, that takes in a vector of numbers as an argument. The function should return the maximum
 //sum of non-adjacent elements in the vector. There is no limit on how manu elements can be taken into the sum as long as
@@ -12,30 +13,44 @@
 // The maximum non-adjacent sum is 16, because 4 + 12. 
 // 4 and 12 are not adjacent in the vector.
 
+int nonAdjacentSum(std::vector<int> nums, int i, std::unordered_map<int, int> &memo) {
+    if(memo.count(i) > 0){
+        return memo[i];
+    }
+    if(i >= nums.size()){
+        return 0;
+    }
+
+    int include = nums[i] + nonAdjacentSum(nums, i + 2, memo);
+    int exclude = nonAdjacentSum(nums, i + 1, memo);
+    memo[i] = std::max(include, exclude);
+    return memo[i];
+}
+
 int nonAdjacentSum(std::vector<int> nums) {
-    // todo
-    return;
+    std::unordered_map<int, int> memo;
+    return nonAdjacentSum(nums, 0, memo);
 }
 
 int main() {
     std::vector<int> nums { 2, 4, 5, 12, 7 };
     nonAdjacentSum(nums); // -> 16
     
-    std::vector<int> nums { 7, 5, 5, 12 };
-    nonAdjacentSum(nums); // -> 19
+    std::vector<int> nums1 { 7, 5, 5, 12 };
+    nonAdjacentSum(nums1); // -> 19
     
-    std::vector<int> nums { 7, 5, 5, 12, 17, 29 };
-    nonAdjacentSum(nums); // -> 48
+    std::vector<int> nums2 { 7, 5, 5, 12, 17, 29 };
+    nonAdjacentSum(nums2); // -> 48
     
-    std::vector<int> nums { 
+    std::vector<int> nums3 { 
         72, 62, 10,  6, 20, 19, 42,
         46, 24, 78, 30, 41, 75, 38,
         23, 28, 66, 55, 12, 17, 9,
         12, 3, 1, 19, 30, 50, 20
     };
-    nonAdjacentSum(nums); // -> 488
+    nonAdjacentSum(nums3); // -> 488
     
-    std::vector<int> nums { 
+    std::vector<int> nums4 { 
         72, 62, 10,  6, 20, 19, 42, 46, 24, 78,
         30, 41, 75, 38, 23, 28, 66, 55, 12, 17,
         83, 80, 56, 68,  6, 22, 56, 96, 77, 98,
@@ -44,7 +59,7 @@ int main() {
         72, 62, 10,  6, 20, 19, 42, 46, 24, 78,
         42
     };
-    nonAdjacentSum(nums); // -> 1465
+    nonAdjacentSum(nums4); // -> 1465
 }
 
 //Complexity
